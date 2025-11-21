056dbc388cdffed8cb79c263a6e3203d6c26fa2f
sofgiman <62728830+sofgiman@users.noreply.github.com>
2025-11-21 23:58:37 +0200

    wip

--- terraform/infrastrucutre/prod/main.tf
+++ terraform/infrastrucutre/prod/main.tf
@@ -7,6 +7,7 @@ terraform {
     }
   }
 }
+
 provider "aws" {
   region = "us-east-1"
 }
@@ -18,6 +19,5 @@ module "network" {
   region              = var.region
   vpc_cidr            = var.vpc_cidr
   public_subnet_cidrs = ["10.0.10.0/24", "10.0.11.0/24"]
-
 }
 
