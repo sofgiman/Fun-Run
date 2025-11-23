fd8aed39e0e9b974cc6732d1bb59f10f6b555131
sofgiman <62728830+sofgiman@users.noreply.github.com>
2025-11-23 22:11:31 +0200

    Feat/add ecs (#34)
    * Added execute in ecs and reverted to ful build pipeline
    
    * added exec role name to tf
    
    * add role name to output

--- terraform/infrastructure/prod/outputs.tf
+++ terraform/infrastructure/prod/outputs.tf
@@ -12,3 +12,18 @@ output "internet_gateway_id" {
   description = "The ID of the internet gateway"
   value       = module.network.internet_gateway_id
 }
+
+output "ecs_cluster_id" {
+  description = "ECS cluster ID"
+  value       = module.ecs_infra.ecs_cluster_id
+}
+
+output "ecs_execution_role_arn" {
+  description = "ECS task arn"
+  value       = module.ecs_infra.ecs_execution_role_arn
+}
+
+output "ecs_execution_role_arn" {
+  description = "ECS task arn"
+  value       = module.ecs_infra.ecs_execution_role_name
+}
