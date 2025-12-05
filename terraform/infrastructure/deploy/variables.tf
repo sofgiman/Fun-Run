d27cf737bf5bbe329faf096846256d60219def2c
sofgiman <62728830+sofgiman@users.noreply.github.com>
2025-12-05 22:34:08 +0200

    Feat/add ecs (#41)
    * Added execute in ecs and reverted to ful build pipeline
    
    * added exec role name to tf
    
    * add role name to output
    
    * fix typo
    
    * add task role
    
    * fixed naming errors
    
    * added git_commit_hash to docker labels and fixed build error
    
    * fixed naming
    
    * debug
    
    * final fix to naming
    
    * wip
    
    * wip
    
    * switch to single directory pattern with dynamic backend config

---
+++ terraform/infrastructure/deploy/variables.tf
@@ -0,0 +1,32 @@
+variable "project" {
+  description = "fun-run"
+  type        = string
+  default     = "fun-run"
+}
+
+variable "environment" {
+  description = "Environment name (dev, prod)"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.0.0/24", "10.0.1.0/24"]
+}
+
+variable "enable_execute_command" {
+  type    = bool
+  default = false
+}
