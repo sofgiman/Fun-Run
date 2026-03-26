a74f2716c739c6759df69c37232cac4020af3270
sofgiman <62728830+sofgiman@users.noreply.github.com>
2026-03-26 10:59:43 +0200

    chore: add terraform lock files for linux_amd64 and darwin_arm64

--- terraform/server/deploy/main.tf
+++ terraform/server/deploy/main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
 data "terraform_remote_state" "infra" {
   backend = "s3"
   config = {
