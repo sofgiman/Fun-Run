8dbf7723e9407628a4d7464383073b59371b6c5b
sofgiman <62728830+sofgiman@users.noreply.github.com>
2026-03-29 14:57:54 +0300

    fix: distribute subnets across AZs and enable cross-zone load balancing

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -2,6 +2,9 @@ provider "aws" {
   region = var.region
 }
 
+data "aws_availability_zones" "available" {
+  state = "available"
+}
 
 # VPC
 resource "aws_vpc" "this" {
@@ -17,6 +20,9 @@ resource "aws_subnet" "public" {
   count                   = length(var.public_subnet_cidrs)
   vpc_id                  = aws_vpc.this.id
   cidr_block              = var.public_subnet_cidrs[count.index]
+ 
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+ 
   map_public_ip_on_launch = true
 
   tags = {
