866c68db4dfd8170220daccd3760e0462509976a
sofgiman <62728830+sofgiman@users.noreply.github.com>
2026-03-30 11:37:52 +0300

    Update main.tf

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -8,7 +8,9 @@ data "aws_availability_zones" "available" {
 
 # VPC
 resource "aws_vpc" "this" {
-  cidr_block = var.vpc_cidr
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
   tags = {
     Name = "${var.project}-${var.environment}-vpc"
